f5004654eab0ee17696416e2b00caf3d6f778e22
deMuNeS <136003237+deMuNeS@users.noreply.github.com>
2024-07-15 16:43:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$('form').on('submit', function(evento) {
+    evento.preventDefault();
+    const novaTarefa = $('#nome-da-tarefa').val();
+    const novaLinhaLista = $(`<li class="novaTarefa"></li>`);
+    const botaoTarefa = $(`<a>${novaTarefa}</a>`).appendTo($('<div></div>').appendTo(novaLinhaLista));
+    $(novaLinhaLista).appendTo('ul');
+    $('#tarefas-titulo').fadeIn(200, function() {
+        $(novaLinhaLista).fadeIn(500);
+    });
+    $('#nome-da-tarefa').val('');
+});
