777aa47de369b1e8fee10b3232c5aca9cf3c9a32
deMuNeS <136003237+deMuNeS@users.noreply.github.com>
2024-07-15 16:46:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ $('form').on('submit', function(evento) {
     const novaTarefa = $('#nome-da-tarefa').val();
     const novaLinhaLista = $(`<li class="novaTarefa"></li>`);
     const botaoTarefa = $(`<a>${novaTarefa}</a>`).appendTo($('<div></div>').appendTo(novaLinhaLista));
+    novaLinhaLista.on('click', function() {
+    $(this).find('a').toggleClass('tarefaRealizada');
+    });
     $(novaLinhaLista).appendTo('ul');
     $('#tarefas-titulo').fadeIn(200, function() {
         $(novaLinhaLista).fadeIn(500);
